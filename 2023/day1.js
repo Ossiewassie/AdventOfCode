4f810cfedbf6ab16026bb955e938d4e81c751895
ossiewassie <ossiewassie@gmail.com>
2023-12-12 21:54:17 +0100

    oops

--- 2023/day1.js
+++ 2023/day1.js
@@ -8,21 +8,18 @@ let myInterface = readline.createInterface({
 });
 
 let sum = 0;
-let writtenNrs = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 myInterface.on('line', function (line) {
     chars = line.split('');
     number1 = "";
     number2 = "";
 
-    (chars).forEach(function(char, i) {
+    (chars).forEach(char => {
         if(char >= 0 && char <= 9 && number1 == ""){
             number1 = char;
-            number1pos = i;
         }
         else if(char >= 0 && char <= 9) {
             number2 = char;
-            number2pos = i;
         }
     });
 
